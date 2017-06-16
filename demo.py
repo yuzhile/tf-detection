71692a1dc9c751a606f78b2e9d80e8ef7e696dfb
yuzhile <yuzhile_study@163.com>
2017-06-16 15:16:19 +0800

    demot

--- demo.py
+++ demo.py
@@ -1,14 +1,33 @@
 from  __future__ import absolute_import
 import tensorflow as tf
+from  config import *
+from nets import * 
 
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_string('checkpoint','./data/model.ckpt-87000','Path to the model parameter file.')
+tf.app.flags.DEFINE_string('checkpoint','./data/model_checkpoints/squeezeDet/model.ckpt-87000','Path to the model parameter file.')
 tf.app.flags.DEFINE_string('input_path','./data/sample.png','Input image.')
 tf.app.flags.DEFINE_string('out_dir','./data/out','Directory to dump output image.')
 tf.app.flags.DEFINE_string('demo_net','squeezeDet','Neural net architecture.')
+tf.app.flags.DEFINE_integer('gpu',-1,'denate the gpu id.')
+
 def image_demo():
-    pass 
+    assert FLAGS.demo_net == 'squeezeDet' or FLAGS.demo_net == 'squeezeDet+',\
+    'Selected nueral net architecture not supported: {}'.format(FLAGS.demo_net)
+    with tf.Graph().as_default():
+        #Load model
+        if FLAGS.demo_net == 'squeezeDet':
+            m = kitti_squeezeDet_config()
+            m.BATCH_SIZE = 1
+            m.LOAD_PRETRAINED_MODEL = False
+            model = SqueezeDet(m,FLAGS.gpu)
+        elif FLAGS.demo_net == 'squeezeDet+':
+            m = kitti_squeezeDetPlus_config()
+            m.BATCH_SIZE = 1
+            m.LOAD_PRETRAINED_MODEL = False
+        saver = tf.train.Saver(model.model_params)
+        with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
+            saver.restore(sess,FLAGS.checkpoint)
 def main(argv=None):
     print 'your are in main'
     if not tf.gfile.Exists(FLAGS.out_dir):
